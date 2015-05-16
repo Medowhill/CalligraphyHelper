bf75fd0d141d92c5efdea1990e80f523e86bad16
Jaemin Hong <hjm0901@naver.com>
2015-05-16 12:01:24 +0900

    글씨 추가 인터페이스 개선, 글씨 추가 버튼 추가

--- app/src/main/java/com/hwajung/ksa/calligraphyhelper/activity/SketchActivity.java
+++ app/src/main/java/com/hwajung/ksa/calligraphyhelper/activity/SketchActivity.java
@@ -6,8 +6,11 @@ import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
 import com.hwajung.ksa.calligraphyhelper.R;
@@ -21,11 +24,12 @@ public class SketchActivity extends Activity {
     final int STATE_MENU_VISIBLE = 0, STATE_MENU_INVISIBLE = 1, STATE_MENU_APPEARING = 2, STATE_MENU_DISAPPEARING = 3;
 
     SketchView sketchView;
-    Button imageButton_menu, imageButton_undo, imageButton_redo;
+    Button button_menu, button_undo, button_redo, button_cancel, button_add;
     MultiButton multiButton_menu;
     GridView gridView_letter;
-
+    ImageView imageView_letterPreview;
     ScrollView scrollView_menu;
+    LinearLayout linearLayout_letter;
 
     Animation animation_menu_appear, animation_menu_disappear;
 
@@ -39,18 +43,37 @@ public class SketchActivity extends Activity {
         Log.i("TEST", "go");
 
         sketchView = (SketchView) findViewById(R.id.sketchView);
-        imageButton_menu = (Button) findViewById(R.id.button_menu);
-        imageButton_redo = (Button) findViewById(R.id.button_redo);
-        imageButton_undo = (Button) findViewById(R.id.button_undo);
+        button_menu = (Button) findViewById(R.id.button_menu);
+        button_redo = (Button) findViewById(R.id.button_redo);
+        button_undo = (Button) findViewById(R.id.button_undo);
+        button_cancel = (Button) findViewById(R.id.button_cancel);
+        button_add = (Button) findViewById(R.id.button_add);
         multiButton_menu = (MultiButton) findViewById(R.id.multiButton_menu);
         gridView_letter = (GridView) findViewById(R.id.gridView_letter);
-
+        imageView_letterPreview = (ImageView) findViewById(R.id.imageView_letterPreview);
         scrollView_menu = (ScrollView) findViewById(R.id.scrollView_menu);
+        linearLayout_letter = (LinearLayout) findViewById(R.id.linearLayout_letter);
 
         sketchView.setSketchActivity(this);
 
-        LetterAdapter letterAdapter = new LetterAdapter(this);
+        final LetterAdapter letterAdapter = new LetterAdapter(this);
         gridView_letter.setAdapter(letterAdapter);
+
+        gridView_letter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                imageView_letterPreview.setVisibility(View.VISIBLE);
+                imageView_letterPreview.setImageResource((int) letterAdapter.getItem(i));
+                button_add.setEnabled(true);
+            }
+        });
+
+        gridView_letter.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                return false;
+            }
+        });
 
         animation_menu_appear = AnimationUtils.loadAnimation(this, R.anim.sketch_menu_appear);
         animation_menu_appear.setAnimationListener(new Animation.AnimationListener() {
@@ -90,7 +113,7 @@ public class SketchActivity extends Activity {
             }
         });
 
-        imageButton_menu.setOnClickListener(new View.OnClickListener() {
+        button_menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 switch (menuAnimation) {
@@ -108,17 +131,26 @@ public class SketchActivity extends Activity {
             }
         });
 
-        imageButton_redo.setOnClickListener(new View.OnClickListener() {
+        button_redo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 sketchView.redo();
             }
         });
 
-        imageButton_undo.setOnClickListener(new View.OnClickListener() {
+        button_undo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 sketchView.undo();
+            }
+        });
+
+        button_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gridView_letter.setVisibility(View.INVISIBLE);
+                imageView_letterPreview.setVisibility(View.INVISIBLE);
+                linearLayout_letter.setVisibility(View.INVISIBLE);
             }
         });
 
@@ -129,24 +161,28 @@ public class SketchActivity extends Activity {
         multiButton_menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                scrollView_menu.startAnimation(animation_menu_disappear);
                 gridView_letter.setVisibility(View.VISIBLE);
+                linearLayout_letter.setVisibility(View.VISIBLE);
+                button_add.setEnabled(false);
             }
         }, 2);
 
     }
 
     public void setUndoButton(boolean enabled) {
-        imageButton_undo.setEnabled(enabled);
+        button_undo.setEnabled(enabled);
     }
 
     public void setRedoButton(boolean enabled) {
-        imageButton_redo.setEnabled(enabled);
+        button_redo.setEnabled(enabled);
     }
 
     public void closeMenu() {
         if (menuAnimation == STATE_MENU_VISIBLE)
             scrollView_menu.startAnimation(animation_menu_disappear);
         gridView_letter.setVisibility(View.INVISIBLE);
+        button_cancel.setVisibility(View.INVISIBLE);
     }
 
 }
